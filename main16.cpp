a36251d2d9c011aa8b6111cd3b2aeccdff153b93
Nina Sidorenko <ndsidorenko@edu.hse.ru>
2024-06-04 05:15:14 -0400

    modified tests for lab16

--- main16.cpp
+++ main16.cpp
@@ -2,115 +2,151 @@
 #include <chrono>
 #include "multmatrix.hpp"
 
-#define THREADS 4
+#define THREADS 6
 
 int main()
 {
   std::cout << "EQUAL NUMBERS OF THREADS" << std::endl;
   std::cout << "Addition:" << std::endl;
-  for (int i = 1; i < 11; ++i)
+  int suma [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int k = 0; k < 3; ++k)
   {
 
-    Matrix <double> m1 (3*i, 3*i);
-    m1.randomise();
+    for (int i = 1; i < 11; ++i)
+    {
 
-    Matrix <double> m2 (3*i, 3*i);
-    m2. randomise();
+      Matrix <double> m1 (3*i, 3*i);
+      m1.randomise();
 
-    const auto start = std::chrono::high_resolution_clock::now();
+      Matrix <double> m2 (3*i, 3*i);
+      m2. randomise();
 
-    Matrix <double> res1 = m1.multithreading_addition (m2, THREADS);
+      const auto start = std::chrono::high_resolution_clock::now();
 
-    const auto stop = std::chrono::high_resolution_clock::now();
+      Matrix <double> res1 = m1.multithreading_addition (m2, THREADS);
 
-    auto elapsed = (stop - start);
+      const auto stop = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Size is " << 3*i << " * "<< 3*i <<". Time is " << elapsed.count() << " ms"<< std::endl;
+      auto elapsed = (stop - start);
+
+      suma[i-1]+= elapsed.count();
+    }
   }
+  for (int i = 0; i <10; ++i)
+    std::cout << "Size is " << 3*(i+1) <<". Time is " << suma[i]/3 << " ms"<< std::endl;
 
   std::cout << "Substraction:"<< std::endl;
-  for (int i = 1; i < 11; ++i)
+  int sums [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int k = 0; k < 3; ++k)
   {
 
-    Matrix <double> m1 (3*i, 3*i);
-    m1.randomise();
 
-    Matrix <double> m2 (3*i, 3*i);
-    m2. randomise();
+    for (int i = 1; i < 11; ++i)
+    {
 
-    const auto start = std::chrono::high_resolution_clock::now();
+      Matrix <double> m1 (3*i, 3*i);
+      m1.randomise();
 
-    Matrix <double> res1 = m1.multithreading_substraction (m2, THREADS);
+      Matrix <double> m2 (3*i, 3*i);
+      m2. randomise();
 
-    const auto stop = std::chrono::high_resolution_clock::now();
+      const auto start = std::chrono::high_resolution_clock::now();
 
-    auto elapsed = (stop - start);
+      Matrix <double> res1 = m1.multithreading_substraction (m2, THREADS);
 
-    std::cout << "Size is " << 3*i << " * "<< 3*i <<". Time is " << elapsed.count() << " ms"<< std::endl;
+      const auto stop = std::chrono::high_resolution_clock::now();
+
+      auto elapsed = (stop - start);
+
+      sums[i-1]+= elapsed.count();
+    }
   }
+  for (int i = 0; i <10; ++i)
+    std::cout << "Size is " << 3*(i+1) <<". Time is " << sums[i]/3 << " ms"<< std::endl;
 
   std::cout << "Scalar multiplication:"<< std::endl;
-  for (int i = 1; i < 11; ++i)
+  int sumsm [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int k = 0; k < 3; ++k)
   {
 
-    Matrix <double> m1 (3*i, 3*i);
-    m1.randomise();
+    for (int i = 1; i < 11; ++i)
+    {
 
-    Matrix <double> m2 (3*i, 3*i);
-    m2. randomise();
+      Matrix <double> m1 (3*i, 3*i);
+      m1.randomise();
 
-    const auto start = std::chrono::high_resolution_clock::now();
+      Matrix <double> m2 (3*i, 3*i);
+      m2. randomise();
 
-    Matrix <double> res1 = m1.multithreading_scalar (2, THREADS);
+      const auto start = std::chrono::high_resolution_clock::now();
 
-    const auto stop = std::chrono::high_resolution_clock::now();
+      Matrix <double> res1 = m1.multithreading_scalar (2, THREADS);
 
-    auto elapsed = (stop - start);
+      const auto stop = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Size is " << 3*i << " * "<< 3*i <<". Time is " << elapsed.count() << " ms"<< std::endl;
+      auto elapsed = (stop - start);
+
+      sumsm[i-1]+= elapsed.count();
+    }
   }
+  for (int i = 0; i <10; ++i)
+    std::cout << "Size is " << 3*(i+1) <<". Time is " << sumsm[i]/3 << " ms"<< std::endl;
 
   std::cout << "Matrix multiplication:"<<std::endl;
-  for (int i = 1; i < 11; ++i)
+  int summ [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int k = 0; k < 3; ++k)
   {
 
-    Matrix <double> m1 (3*i, 3*i);
-    m1.randomise();
+    for (int i = 1; i < 11; ++i)
+    {
 
-    Matrix <double> m2 (3*i, 3*i);
-    m2. randomise();
+      Matrix <double> m1 (3*i, 3*i);
+      m1.randomise();
 
-    const auto start = std::chrono::high_resolution_clock::now();
+      Matrix <double> m2 (3*i, 3*i);
+      m2. randomise();
 
-    Matrix <double> res1 = m1.multithreading_multiplication (m2, THREADS);
+      const auto start = std::chrono::high_resolution_clock::now();
 
-    const auto stop = std::chrono::high_resolution_clock::now();
+      Matrix <double> res1 = m1.multithreading_multiplication (m2, THREADS);
 
-    auto elapsed = (stop - start);
+      const auto stop = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Size is " << 3*i << " * "<< 3*i <<". Time is " << elapsed.count() << " ms"<< std::endl;
+      auto elapsed = (stop - start);
+
+      summ[i-1]+= elapsed.count();
+    }
   }
+  for (int i = 0; i <10; ++i)
+    std::cout << "Size is " << 3*(i+1) <<". Time is " << summ[i]/3 << " ms"<< std::endl;
 
   std::cout << "Transposition:"<< std::endl;
-  for (int i = 1; i < 11; ++i)
+  int sumt [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int k = 0; k < 3; ++k)
   {
 
-    Matrix <double> m1 (3*i, 3*i);
-    m1.randomise();
+    for (int i = 1; i < 11; ++i)
+    {
 
-    Matrix <double> m2 (3*i, 3*i);
-    m2. randomise();
+      Matrix <double> m1 (3*i, 3*i);
+      m1.randomise();
 
-    const auto start = std::chrono::high_resolution_clock::now();
+      Matrix <double> m2 (3*i, 3*i);
+      m2. randomise();
 
-    Matrix <double> res1 = m1.multithreading_transpose (THREADS);
+      const auto start = std::chrono::high_resolution_clock::now();
 
-    const auto stop = std::chrono::high_resolution_clock::now();
+      Matrix <double> res1 = m1.multithreading_transpose (THREADS);
 
-    auto elapsed = (stop - start);
+      const auto stop = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Size is " << 3*i << " * "<< 3*i <<". Time is " << elapsed.count() << " ms"<< std::endl;
+      auto elapsed = (stop - start);
+
+      sumt[i-1]+= elapsed.count();
+    }
   }
+  for (int i = 0; i <10; ++i)
+    std::cout << "Size is " << 3*(i+1) <<". Time is " << sumt[i]/3 << " ms"<< std::endl;
 
   std::cout << std::endl << "EQUAL SIZES OF MATRIXES" << std::endl;
 
@@ -121,79 +157,26 @@ int main()
   m2. randomise();
 
   std::cout << "Addition:" << std::endl;
-  for (int i = 1; i < 11; ++i)
+  int summa [10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (int k = 0; k < 100; ++k)
   {
+    for (int i = 1; i < 11; ++i)
+    {
 
-    const auto start = std::chrono::high_resolution_clock::now();
+      const auto start = std::chrono::high_resolution_clock::now();
 
-    Matrix <double> res1 = m1.multithreading_addition (m2, i);
+      Matrix <double> res1 = m1.multithreading_addition (m2, i);
 
-    const auto stop = std::chrono::high_resolution_clock::now();
+      const auto stop = std::chrono::high_resolution_clock::now();
 
-    auto elapsed = (stop - start);
+      auto elapsed = (stop - start);
 
-    std::cout << "Number of threads " << i << ". Time is " << elapsed.count() << " ms"<< std::endl;
+      summa[i-1] += elapsed.count();
+    }
   }
+  for (int i = 0; i < 10; ++i)
+    std::cout << "Number of threads " << i+1 << ". Time is " << summa[i]/3 << " ms"<< std::endl;
 
-  std::cout << "Substraction:" << std::endl;
-  for (int i = 1; i < 11; ++i)
-  {
-
-    const auto start = std::chrono::high_resolution_clock::now();
-
-    Matrix <double> res1 = m1.multithreading_substraction (m2, i);
-
-    const auto stop = std::chrono::high_resolution_clock::now();
-
-    auto elapsed = (stop - start);
-
-    std::cout << "Number of threads " << i << ". Time is " << elapsed.count() << " ms"<< std::endl;
-  }
-
-  std::cout << "Scalar multiplication:" << std::endl;
-  for (int i = 1; i < 11; ++i)
-  {
-
-    const auto start = std::chrono::high_resolution_clock::now();
-
-    Matrix <double> res1 = m1.multithreading_scalar (2, i);
-
-    const auto stop = std::chrono::high_resolution_clock::now();
-
-    auto elapsed = (stop - start);
-
-    std::cout << "Number of threads " << i << ". Time is " << elapsed.count() << " ms"<< std::endl;
-  }
-
-  std::cout << "Matrix multiplication:" << std::endl;
-  for (int i = 1; i < 11; ++i)
-  {
-
-    const auto start = std::chrono::high_resolution_clock::now();
-
-    Matrix <double> res1 = m1.multithreading_multiplication (m2, i);
-
-    const auto stop = std::chrono::high_resolution_clock::now();
-
-    auto elapsed = (stop - start);
-
-    std::cout << "Number of threads " << i << ". Time is " << elapsed.count() << " ms"<< std::endl;
-  }
-
-  std::cout << "Transposition:" << std::endl;
-  for (int i = 1; i < 11; ++i)
-  {
-
-    const auto start = std::chrono::high_resolution_clock::now();
-
-    Matrix <double> res1 = m1.multithreading_transpose (i);
-
-    const auto stop = std::chrono::high_resolution_clock::now();
-
-    auto elapsed = (stop - start);
-
-    std::cout << "Number of threads " << i << ". Time is " << elapsed.count() << " ms"<< std::endl;
-  }
 
   return 0;
 }
